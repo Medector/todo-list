aa0f2fb8ce52e8c3b65aa5272357bf1f6542f31a
Hector Medina <97632503+Medina-Hector@users.noreply.github.com>
2022-09-15 23:17:48 -0500

    deploy v1.2

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,22 +1,17 @@
 import React from "react";
-import "./CreateTodoButton.css"
+import "./CreateTodoButton.css";
+import AddIcon from "@mui/icons-material/Add";
+function CreateTodoButton(props) {
+  const onClickTodoButton = () => {
+    props.setOpenModal((prevState) => !prevState);
+    // props.setOpenModal.toggle(false)
+  };
 
-function CreateTodoButton (props) {
-    
-    const onClickTodoButton = () => {
-        props.setOpenModal(prevState => !prevState);
-        // props.setOpenModal.toggle(false)
-
-    }
-    
-    return(
-        <button 
-        className="CreateTodoButton"
-        onClick={onClickTodoButton}
-        >    
-            +
-        </button>
-    )
+  return (
+    <button className="CreateTodoButton" onClick={onClickTodoButton}>
+      +
+    </button>
+  );
 }
 
-export {CreateTodoButton};
+export { CreateTodoButton };
--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,29 +1,25 @@
 import React from "react";
-import "./TodoItem.css"
-
-
-function TodoItem(props){
-    React.useEffect(() => console.log(props.completed), [])
-    return(
-        
-        <li className="TodoItem">
-        <span
-            className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-            onClick={props.onComplete}
-        >
-            x
-        </span>
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>        
-         {props.text} 
-         </p>
-       <span
-            className="Icon Icon-delete"
-            onClick={props.onDelete}
-       >
-           x
-       </span>
-       </li>
-    );
+import "./TodoItem.css";
+import CheckIcon from "@mui/icons-material/Check";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+function TodoItem(props) {
+  React.useEffect(() => console.log(props.completed), []);
+  return (
+    <li className="TodoItem" id="TodoItemBox">
+      <span
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.onComplete}
+      >
+        <CheckIcon id="checkIcon" />
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+      <span className="Icon Icon-delete" onClick={props.onDelete}>
+        <DeleteForeverIcon id="deleteIcon" />
+      </span>
+    </li>
+  );
 }
 
-export {TodoItem};
+export { TodoItem };
